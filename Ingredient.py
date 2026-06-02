447bb6de2a76d5fd0584f6542140ba0b6392a6d2
kuzya <kuzyadana30.11@gmail.com>
2026-06-02 16:58:06 +0300

    Создала класс Ingredient

--- Ingredient.py
+++ Ingredient.py
@@ -3,3 +3,25 @@ class Ingredient:
         self.name = Name
         self.quantity = Quantity
         self.unit = Unit
+
+    @property
+    def quantity(self):
+        return self.quantity
+
+    @property.setter
+    def quantity(self, quan):
+        if quan <= 0:
+            raise ValueError("Количество должно быть положительным")
+        self.quantity = float(quan)
+
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+
+    def __eq__(self, other):
+        if isinstance(other, Ingredient):
+            if self.name == other.name and self.unit == other.unit:
+                return True
+        return False
